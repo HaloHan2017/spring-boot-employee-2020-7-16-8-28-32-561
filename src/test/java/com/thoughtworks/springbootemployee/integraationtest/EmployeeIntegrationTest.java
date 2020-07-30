2367b8194db7695a078d8ca29be843d28105c3c9
HaloHan2017 <2332722908@qq.com>
2020-07-30 16:04:44 +0800

    [springboot-employees] [Steve,Jerry] feature : add EmployeeIntegrationTest and should_return_employee_when_add_employee_given_employee method

---
+++ src/test/java/com/thoughtworks/springbootemployee/integraationtest/EmployeeIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.thoughtworks.springbootemployee.integraationtest;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @BeforeEach
+    void clean() {
+        employeeRepository.deleteAll();
+    }
+
+    @Test
+    void should_return_employee_when_add_employee_given_employee() throws Exception {
+        String employee = "{\n" +
+                "        \"name\":\"dasdas\",\n" +
+                "        \"age\":23,\n" +
+                "        \"gender\":\"male\",\n" +
+                "        \"salary\":312321,\n" +
+                "        \"companyId\":33\n" +
+                "}";
+        mockMvc.perform(post("/employees").contentType(MediaType.APPLICATION_JSON).content(employee))
+                .andExpect((status().isCreated()))
+                .andExpect(jsonPath("$.name").value("dasdas"));
+
+        List<Employee> employees = employeeRepository.findAll();
+        assertEquals(1, employees.size());
+        assertEquals("dasdas",employees.get(0).getName());
+    }
+}
