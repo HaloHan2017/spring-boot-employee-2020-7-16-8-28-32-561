a0403f98457bd87c6f98100ac943923c2a7be760
HaloHan2017 <2332722908@qq.com>
2020-07-30 16:38:45 +0800

    [springboot-employees] [Steve,Jerry] refactor: delete unused import

--- src/test/java/com/thoughtworks/springbootemployee/integraationtest/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integraationtest/EmployeeIntegrationTest.java
@@ -9,10 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.util.List;
-import java.util.Optional;
-
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
