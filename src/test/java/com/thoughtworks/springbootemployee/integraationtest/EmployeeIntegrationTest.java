9974ac7ad41adbd54d87adc4bf74a1017c4a8cab
HaloHan2017 <2332722908@qq.com>
2020-07-30 16:12:34 +0800

    [springboot-employees] [Steve,Jerry] feature : add should_return_all_employees_when_get_employees_given_none method

--- src/test/java/com/thoughtworks/springbootemployee/integraationtest/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integraationtest/EmployeeIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -41,11 +42,19 @@ public class EmployeeIntegrationTest {
                 "        \"companyId\":33\n" +
                 "}";
         mockMvc.perform(post("/employees").contentType(MediaType.APPLICATION_JSON).content(employee))
-                .andExpect((status().isCreated()))
+                .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.name").value("dasdas"));
 
         List<Employee> employees = employeeRepository.findAll();
         assertEquals(1, employees.size());
         assertEquals("dasdas",employees.get(0).getName());
+    }
+
+    @Test
+    void should_return_all_employees_when_get_employees_given_none() throws Exception {
+        employeeRepository.save(new Employee(1, "JACK", 18, "male", 1000));
+        mockMvc.perform(get("/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("JACK"));
     }
 }
